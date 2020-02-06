200f8de2cd9d93b4e8884213caa5deafa2268706
Benjamin Main <bmain@lumeris.com>
2020-02-06 12:30:16 -0600

    refactor(AddTests): Add Unit Tests.

---
+++ src/example/example-qldb-viewset.spec.ts
@@ -0,0 +1,52 @@
+import { QldbSession, TransactionExecutor } from 'amazon-qldb-driver-nodejs';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ExampleModel } from './example.model';
+import { ExampleQldbViewset } from './example-qldb-viewset';
+import { QLDB_SESSION_TOKEN } from '../tokens';
+
+describe('ExampleQldbViewset', () => {
+  let subject: ExampleQldbViewset;
+  let session: QldbSession;
+  let executeLambdaSpy: jest.SpyInstance<
+    Promise<any>,
+    [
+      (transactionExecutor: TransactionExecutor) => any,
+      ((retryAttempt: number) => void)?,
+    ]
+  >;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ExampleQldbViewset,
+        {
+          provide: QLDB_SESSION_TOKEN,
+          useValue: {
+            executeLambda: () => null,
+          },
+        },
+      ],
+    }).compile();
+    subject = module.get<ExampleQldbViewset>(ExampleQldbViewset);
+    session = module.get<QldbSession>(QLDB_SESSION_TOKEN);
+    executeLambdaSpy = jest.spyOn(session, 'executeLambda');
+  });
+
+  it('should be defined', () => {
+    expect(subject).toBeTruthy();
+  });
+  describe('retrieve()', () => {
+    it('should retrieve by key', async () => {
+      const mockResponse: ExampleModel = {
+        name: 'Ben',
+        age: 32,
+        gender: 'male',
+      };
+      executeLambdaSpy.mockReturnValue(Promise.resolve(mockResponse));
+      const response = await subject.retrieve('111');
+      expect(response).toEqual(mockResponse);
+      expect(executeLambdaSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
